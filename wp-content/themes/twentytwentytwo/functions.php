e25962d29c4c569c483118321d52daf14f8536eb
Egor <84025024+egornmore@users.noreply.github.com>
2024-11-13 21:40:29 +0200

    Update functions.php

--- wp-content/themes/twentytwentytwo/functions.php
+++ wp-content/themes/twentytwentytwo/functions.php
@@ -1,3 +1,19 @@
 <?php
 
 
+if ( ! function_exists( 'twentytwentytwo_support' ) ) :
+
+	function twentytwentytwo_support() {
+
+		// Add support for block styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Enqueue editor styles.
+		add_editor_style( 'style.css' );
+	}
+
+endif;
+
+add_action( 'after_setup_theme', 'twentytwentytwo_support' );
+
+
