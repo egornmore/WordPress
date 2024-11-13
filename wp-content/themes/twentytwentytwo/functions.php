3a91cce21579846dfea22a8bf08e6844bc5117ae
Egor <84025024+egornmore@users.noreply.github.com>
2024-11-13 22:20:42 +0200

    Update functions.php

--- wp-content/themes/twentytwentytwo/functions.php
+++ wp-content/themes/twentytwentytwo/functions.php
@@ -8,6 +8,9 @@ if ( ! function_exists( 'twentytwentytwo_support' ) ) :
 		// Add support for block styles.
 		add_theme_support( 'wp-block-styles' );
 
+		// Enqueue editor styles.
+		add_editor_style('style.css');
+
 	}
 
 endif;
@@ -24,6 +27,14 @@ if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
 
 		$version_string = is_string( $theme_version ) ? $theme_version : false;
 
+		wp_register_style(
+			'twentytwentytwo-style',
+			get_template_directory_uri() . '/style.css',
+			array(),
+			$version_string
+		);
+
+		wp_enqueue_style('twentytwentytwo-style');
 	}
 
 endif;
