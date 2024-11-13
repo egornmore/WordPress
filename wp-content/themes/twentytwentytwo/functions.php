b0ed8f5cbf9618f7846683d17e1c9b24f0405f76
Egor <84025024+egornmore@users.noreply.github.com>
2024-11-13 23:13:10 +0200

    Update functions.php

--- wp-content/themes/twentytwentytwo/functions.php
+++ wp-content/themes/twentytwentytwo/functions.php
@@ -18,28 +18,5 @@ endif;
 add_action( 'after_setup_theme', 'twentytwentytwo_support' );
 
 
-
-if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
-
-	function twentytwentytwo_styles() {
-		// Register theme stylesheet.
-		$theme_version = wp_get_theme()->get( 'Version' );
-
-		$version_string = is_string( $theme_version ) ? $theme_version : false;
-
-		wp_register_style(
-			'twentytwentytwo-style',
-			get_template_directory_uri() . '/style.css',
-			array(),
-			$version_string
-		);
-
-		wp_enqueue_style('twentytwentytwo-style');
-	}
-
-endif;
-
-add_action( 'wp_enqueue_scripts', 'twentytwentytwo_styles' );
-
 // Add block patterns
 require get_template_directory() . '/inc/block-patterns.php';
