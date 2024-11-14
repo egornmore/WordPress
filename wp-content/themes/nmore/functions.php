5cc0725b3313ff706b09a5c14c99cc9bec0e357b
Egor <84025024+egornmore@users.noreply.github.com>
2024-11-14 20:14:20 +0200

    Update functions.php

--- wp-content/themes/nmore/functions.php
+++ wp-content/themes/nmore/functions.php
@@ -1,32 +1,9 @@
 <?php
 
 
-id ( ! function_exists( 'twentytwentytwo_styles' ) ) :
+if ( ! function_exists( 'nmore_support' ) ) :
 
-	function twentytwentytwo_styles() {
-	//Register theme CSS
-	$theme_version = wp_get_theme()->get( 'Version' );
-
-	$version_string = is_string( $theme_version ) ? $theme_version : false;
-
-	wp_register_style(
-		'twentytwentytwo-style'
-		get_template_directory_uri() . '/style.css',
-		array(),
-		$version_string
-	);
-
-	wp_enqueue_style('twentytwentytwo-style');
-}
-
-endif;
-
-add_action( 'wp_enqueue_scripts', 'twentytwentytwo_styles');
-
-
-if ( ! function_exists( 'twentytwentytwo_support' ) ) :
-
-	function twentytwentytwo_support() {
+	function nnmore_support() {
 
 		// Add support for block styles.
 		add_theme_support( 'wp-block-styles' );
@@ -38,7 +15,7 @@ if ( ! function_exists( 'twentytwentytwo_support' ) ) :
 
 endif;
 
-add_action( 'after_setup_theme', 'twentytwentytwo_support' );
+add_action( 'after_setup_theme', 'nmore_support' );
 
 
 // Add block patterns
