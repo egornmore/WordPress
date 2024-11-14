de1704eaa050305a55cfa1f75ee0f84f90fa448b
Egor <84025024+egornmore@users.noreply.github.com>
2024-11-14 20:29:24 +0200

    Update functions.php

--- wp-content/themes/nmore/functions.php
+++ wp-content/themes/nmore/functions.php
@@ -1,5 +1,29 @@
 <?php
 
+if ( ! function_exists( 'nmore_styles' ) ) :
+
+	function nmore_styles() {
+
+		// Register stylesheet
+		$theme_version = wp_get_theme()->get( 'Version' );
+
+		$version_string = is_string( $theme_version ) ? $theme_version : false;
+
+		wp_register_style(
+			'nmore-style',
+			get_template_directory_uri() . '/style.css',
+			array(),
+			$version_string
+		);
+
+		wp_enqueue_style('nmore-style');
+	}
+
+endif;
+
+add_action( 'wp_enqueue_scripts', 'nmore_styles' );
+
+
 
 if ( ! function_exists( 'nmore_support' ) ) :
 
